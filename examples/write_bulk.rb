d0ceaa9ee6fe1f556c900102f49cd26be748b578
gethemant <gethemant@bc05e79a-313a-0410-84ff-a729d6f705cd>
2008-01-14 15:41:53 +0000

    commit fixes for running mongrel and stuff

--- examples/write_bulk.rb
+++ examples/write_bulk.rb
@@ -1,28 +1,29 @@
 require "socket"
 
 sock = TCPSocket.open("localhost",11007)
-data = File.open("netbeans.jpg").read
+# data = File.open("netbeans.jpg").read
+data = File.open("nginx.dat").read
 p data.length
 
-1.times do
-#   sock.write(data)
-#   select([sock],nil,nil,nil)
-#   read_data = ""
+1000.times do
+  #   sock.write(data)
+  #   select([sock],nil,nil,nil)
+  #   read_data = ""
 
-#   loop do
-#     begin
-#       while(read_data << sock.read_nonblock(1023)); end
-#     rescue Errno::EAGAIN
-#       break
-#     rescue
-#       break
-#     end
-#   end
+  #   loop do
+  #     begin
+  #       while(read_data << sock.read_nonblock(1023)); end
+  #     rescue Errno::EAGAIN
+  #       break
+  #     rescue
+  #       break
+  #     end
+  #   end
 
-#   p read_data.length
+  #   p read_data.length
   written_length = sock.write(data)
-  p "Write Length: #{written_length}"
+  #p "Write Length: #{written_length}"
   read_length = sock.read(written_length)
-  p "Read length: #{read_length.length}"
+  #p "Read length: #{read_length.length}"
 end
 
