663f7a083a3280eaafe50da7f0aa171546805f72
gethemant <gethemant@bc05e79a-313a-0410-84ff-a729d6f705cd>
2008-02-11 16:48:47 +0000

    check in new code after removing thread pools

--- trunk/tests/echo_server.rb
+++ trunk/tests/echo_server.rb
@@ -1,8 +1,7 @@
 require File.join(File.dirname(__FILE__) + "/runner")
-require "thread" 
 
 class Foo
-  def receieve_data data
+  def receive_data data
     send_data data
   end
   
@@ -15,6 +14,31 @@ class Foo
   end
 end
 
+class Bar
+  
+  def receive_data data
+    puts "received #{data}" 
+    @count += 1
+  end
+  
+  def post_init
+    @count = 0
+    puts "calling post init in client handler" 
+  end
+  
+  def connection_completed
+    100.times { |x| send_data("lol : #{x}\n")}
+  end
+  
+  def unbind
+    puts "server dropped connection" 
+  end
+end
+
 Packet::Reactor.run do |t_reactor|
   t_reactor.start_server("0.0.0.0",11007,Foo)
+  t_reactor.connect("0.0.0.0",11007,Bar)
 end
+
+
+
--- trunk/tests/runner.rb
+++ trunk/tests/runner.rb
@@ -1,7 +1,4 @@
 APP_ROOT = File.expand_path(File.join(File.dirname(__FILE__) + "/.."))
-["extras","bin","worker","lib"].each { |x| $LOAD_PATH.unshift(EVAL_APP_ROOT + "/#{x}")}
-
-WORKER_ROOT = APP_ROOT + "/worker"
-
+["lib"].each { |x| $LOAD_PATH.unshift(APP_ROOT + "/#{x}")}
 require "packet"
 
